7e174285d182d5cf0b85208cc20ccbde6be78d2d
BWsix <itisbw62@gmail.com>
2022-01-21 15:05:04 +0800

    feat: switched to dark theme

--- app/core/styles/theme.ts
+++ app/core/styles/theme.ts
@@ -1,9 +1,12 @@
 import { red } from "@mui/material/colors";
 import { createTheme } from "@mui/material/styles";
 
-// Create a theme instance.
 const theme = createTheme({
   palette: {
+    mode: "dark",
+    background: {
+      default: "#0e001d",
+    },
     primary: {
       main: "#6700EB",
     },
