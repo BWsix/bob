a5e7df276e16d375826148bfa0f4553054c9d97f
BWsix <itisbw62@gmail.com>
2022-01-23 01:55:21 +0800

    refactor(UI): `pos=sticky` & hide on scroll

--- app/core/components/Header.tsx
+++ app/core/components/Header.tsx
@@ -8,9 +8,11 @@ import Container from "@mui/material/Container";
 import IconButton from "@mui/material/IconButton";
 import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
+import Slide from "@mui/material/Slide";
 import Toolbar from "@mui/material/Toolbar";
 import Tooltip from "@mui/material/Tooltip";
 import Typography from "@mui/material/Typography";
+import useScrollTrigger from "@mui/material/useScrollTrigger";
 import logout from "app/auth/mutations/logout";
 import { useMutation } from "blitz";
 import { Suspense, useState } from "react";
@@ -28,7 +30,18 @@ const menuItems = [
   </Button>,
 ];
 
-export const Header = () => {
+function HideOnScroll(props) {
+  const { children } = props;
+  const trigger = useScrollTrigger();
+
+  return (
+    <Slide appear={false} direction="down" in={!trigger}>
+      {children}
+    </Slide>
+  );
+}
+
+export const Header = (props) => {
   const [logoutMutation] = useMutation(logout);
 
   const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
@@ -51,81 +64,83 @@ export const Header = () => {
   };
 
   return (
-    <AppBar position="static" enableColorOnDark={true}>
-      <Container maxWidth="md">
-        <Toolbar disableGutters>
-          <Typography
-            noWrap
-            variant="h6"
-            component="div"
-            sx={{ mr: 2, display: { xs: "none", md: "flex" } }}
-          >
-            Bob - 自主學習檔案系統
-          </Typography>
+    <HideOnScroll {...props}>
+      <AppBar position="sticky" enableColorOnDark={true}>
+        <Container maxWidth="md">
+          <Toolbar disableGutters>
+            <Typography
+              noWrap
+              variant="h6"
+              component="div"
+              sx={{ mr: 2, display: { xs: "none", md: "flex" } }}
+            >
+              Bob - 自主學習檔案系統
+            </Typography>
 
-          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-            <IconButton size="large" onClick={handleOpenNavMenu} color="inherit">
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              keepMounted
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: "bottom",
-                horizontal: "left",
-              }}
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "left",
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: "block", md: "none" },
-              }}
-            >
-              {menuItems}
-            </Menu>
-          </Box>
-          <Typography
-            noWrap
-            variant="h6"
-            component="div"
-            sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}
-          >
-            Bob - 自主學習檔案系統
-          </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>{menuItems}</Box>
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="開啟選單">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Suspense fallback={<AccountCircleIcon fontSize="large" />}>
-                  <AvatarLoader />
-                </Suspense>
+            <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+              <IconButton size="large" onClick={handleOpenNavMenu} color="inherit">
+                <MenuIcon />
               </IconButton>
-            </Tooltip>
-            <Menu
-              keepMounted
-              sx={{ mt: "45px" }}
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
+              <Menu
+                keepMounted
+                anchorEl={anchorElNav}
+                anchorOrigin={{
+                  vertical: "bottom",
+                  horizontal: "left",
+                }}
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "left",
+                }}
+                open={Boolean(anchorElNav)}
+                onClose={handleCloseNavMenu}
+                sx={{
+                  display: { xs: "block", md: "none" },
+                }}
+              >
+                {menuItems}
+              </Menu>
+            </Box>
+            <Typography
+              noWrap
+              variant="h6"
+              component="div"
+              sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}
             >
-              <MenuItem onClick={async () => await logoutMutation()}>
-                <Typography textAlign="center">登出</Typography>
-              </MenuItem>
-            </Menu>
-          </Box>
-        </Toolbar>
-      </Container>
-    </AppBar>
+              Bob - 自主學習檔案系統
+            </Typography>
+            <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>{menuItems}</Box>
+            <Box sx={{ flexGrow: 0 }}>
+              <Tooltip title="開啟選單">
+                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                  <Suspense fallback={<AccountCircleIcon fontSize="large" />}>
+                    <AvatarLoader />
+                  </Suspense>
+                </IconButton>
+              </Tooltip>
+              <Menu
+                keepMounted
+                sx={{ mt: "45px" }}
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+              >
+                <MenuItem onClick={async () => await logoutMutation()}>
+                  <Typography textAlign="center">登出</Typography>
+                </MenuItem>
+              </Menu>
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </HideOnScroll>
   );
 };
