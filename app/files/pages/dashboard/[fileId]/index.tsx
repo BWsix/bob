a0007d33c72c2f8986d493a2e15ac1726c9c061f
BWsix <itisbw62@gmail.com>
2022-01-23 00:21:58 +0800

    feat(UI): updated file page

--- app/files/pages/dashboard/[fileId]/index.tsx
+++ app/files/pages/dashboard/[fileId]/index.tsx
@@ -1,58 +1,95 @@
-import { Suspense } from "react";
-import { Head, Link, useRouter, useQuery, useParam, BlitzPage, useMutation, Routes } from "blitz";
+import OpenInNewIcon from "@mui/icons-material/OpenInNew";
+import Button from "@mui/material/Button";
+import Chip from "@mui/material/Chip";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import { Breadcrumb } from "app/core/components/Breadcrumb";
+import { Loader } from "app/core/components/gadgets/Loader";
 import Layout from "app/core/layouts/Layout";
-import getFile from "app/files/queries/getFile";
 import deleteFile from "app/files/mutations/deleteFile";
+import getFile from "app/files/queries/getFile";
+import { BlitzPage, Link, Routes, useMutation, useParam, useQuery, useRouter } from "blitz";
+import { Suspense } from "react";
 
-export const File = () => {
+const File = ({ fileId }: { fileId: string }) => {
   const router = useRouter();
-  const fileId = useParam("fileId", "string");
-  const [deleteFileMutation] = useMutation(deleteFile);
+
   const [file] = useQuery(getFile, { fileId });
+  const [deleteFileMutation] = useMutation(deleteFile);
 
   return (
     <>
-      <Head>
-        <title>{file.title}</title>
-      </Head>
+      <Breadcrumb meta={[{ path: "/dashboard", name: "檔案列表" }, { name: file.title }]} />
 
-      <div>
-        <h1>File {file.id}</h1>
-        <pre>{JSON.stringify(file, null, 2)}</pre>
+      <Container component="main" maxWidth="md">
+        <Typography component="h1" variant="h2" color="secondary" marginTop={4} marginBottom={1}>
+          {file.title}
+        </Typography>
 
-        <Link href={Routes.EditFilePage({ fileId: file.id })}>
-          <a>Edit</a>
-        </Link>
+        {file.externalUrl && (
+          <Typography color="secondary">
+            <Chip
+              clickable
+              variant="outlined"
+              component="a"
+              label={file.externalUrl}
+              href={file.externalUrl}
+              target="_blank"
+              deleteIcon={<OpenInNewIcon />}
+              onDelete={() => {}}
+            />
+          </Typography>
+        )}
 
-        <button
-          type="button"
-          onClick={async () => {
-            if (window.confirm("This will be deleted")) {
-              await deleteFileMutation({ fileId: file.id });
-              router.push(Routes.FilesPage());
-            }
-          }}
+        <Typography
+          color="secondary"
+          marginY={8}
+          style={{ display: "inline-block", whiteSpace: "pre-line" }}
         >
-          Delete
-        </button>
-      </div>
+          {file.description}
+        </Typography>
+
+        <Grid container justifyContent="flex-end">
+          <Grid item>
+            <Link href={Routes.EditFilePage({ fileId: file.id })}>
+              <Button variant="text" color="secondary">
+                編輯
+              </Button>
+            </Link>
+            <Button
+              variant="text"
+              color="secondary"
+              onClick={async () => {
+                if (window.confirm("將會刪除檔案 - " + file.title)) {
+                  await deleteFileMutation({ fileId: file.id });
+                  router.push(Routes.FilesPage());
+                }
+              }}
+            >
+              刪除
+            </Button>
+          </Grid>
+          <Grid item md={12}>
+            <Button disabled variant="text" color="secondary" fullWidth>
+              <Typography>最後更新於 {file.updatedAt.toLocaleString()}</Typography>
+            </Button>
+          </Grid>
+        </Grid>
+      </Container>
     </>
   );
 };
 
 const ShowFilePage: BlitzPage = () => {
-  return (
-    <div>
-      <p>
-        <Link href={Routes.FilesPage()}>
-          <a>Files</a>
-        </Link>
-      </p>
+  const fileId = useParam("fileId", "string");
 
-      <Suspense fallback={<div>Loading...</div>}>
-        <File />
+  return (
+    <>
+      <Suspense fallback={<Loader />}>
+        <File fileId={fileId!} />
       </Suspense>
-    </div>
+    </>
   );
 };
 
